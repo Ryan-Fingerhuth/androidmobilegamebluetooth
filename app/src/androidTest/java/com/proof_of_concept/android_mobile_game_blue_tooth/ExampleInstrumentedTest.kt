c8cce36578510a53092f1bcdfae165da887bb84f
ryan <rfingerhuth@gmail.com>
2018-11-10 01:24:40 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/proof_of_concept/android_mobile_game_blue_tooth/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.proof_of_concept.android_mobile_game_blue_tooth
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.proof_of_concept.android_mobile_game_blue_tooth", appContext.packageName)
+    }
+}
